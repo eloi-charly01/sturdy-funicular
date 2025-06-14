61fd86ed27a59d476dc678efc0e82bdd48687903
Eloi Charly <nandry556@gmail.com>
2025-06-15 00:49:29 +0300

    feat : add features  project

---
+++ src/Form/ProjectForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'En cours' => 'En cours',
+                    'Terminé' => 'Terminé',
+                    'Annulé' => 'Annulé',
+                    'En attente' => 'En attente',
+                ]
+            ])
+            ->add('acronyme', TextType::class, [
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
